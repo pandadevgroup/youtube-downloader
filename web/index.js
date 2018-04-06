959ab0edd3b34c1c9042e44a55ae92929bc1f0cf
Nathan Wang <nathan.r.wang@gmail.com>
2018-04-05 21:26:02 -0700

    add very basic parse format code

--- web/index.js
+++ web/index.js
@@ -1,13 +1,13 @@
 (function($) {
-	$(document).ready(function() {
-		let videoInfo;
+	var videoInfo;
 
+	$(document).ready(function() {
 		// TODO:
 		// Add a form with Youtube URL. On submit, 
 		// call onUrlEnter() with the video id (eg. "cyW2ajAVyfA")
 
 		// Just for testing
-		onUrlEnter("c0mX-5q3mrY");
+		onUrlEnter("cyW2ajAVyfA");
 
 		var videoURLRegex = new RegExp("^(?:https?)?:\\/\\/(?:www\\.)?(?:youtube\\.com\\/watch\\?v=|youtu\\.be\\/)(.+)$");
 		$("#url").keyup(function() {
@@ -32,17 +32,37 @@
 			video_id: data.video_id,
 			length_seconds: data.length_seconds,
 			author: data.author,
-			formats: parseFormats(data.formats),
+			formats: parseFormats(data.fmt_list, data.formats),
 			published: data.published,
 			description: data.description,
 			video_url: data.video_url
 		};
 	}
 
-	function parseFormats(formats) {
-		return {
-			
+	function parseFormats(fmt_list, formats) {
+		console.log(fmt_list, formats);
+		var parsed = {};
+		var resolutionToNameMap = {
+			"176x144": "Very Low Quality (144p)",
+			"320x180": "Low Quality (180p)",
+			"640x360": "Medium Quality (360p)",
+			"1280x720": "High Quality (720p)"
 		};
+		for (var i = 0; i < fmt_list.length; i++) {
+			var tag = fmt_list[i][0];
+			var resolution = fmt_list[i][1];
+			var name = resolutionToNameMap[resolution] || "Unknown (" + resolution + ")";
+			console.log(getFormatFromTag(tag, formats));
+			parsed[name] = tag;
+		}
+		return parsed;
+	}
+
+	function getFormatFromTag(tag, formats) {
+		for (var i = 0; i < formats.length; i++) {
+			if (formats[i].itag === tag) return formats[i];
+		}
+		return null;
 	}
 
 	function displayInfo(videoInfo) {
