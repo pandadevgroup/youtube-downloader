e99fa295dee037be425be6ea53dba2ef1c84b344
Jeffrey Meng <jeffkmeng@gmail.com>
2018-04-05 21:38:28 -0700

    basic information

--- web/index.js
+++ web/index.js
@@ -3,19 +3,25 @@
 		let videoInfo;
 
 		// TODO:
-		// Add a form with Youtube URL. On submit, 
+		// Add a form with Youtube URL. On submit,
 		// call onUrlEnter() with the video id (eg. "cyW2ajAVyfA")
 
 		// Just for testing
-		onUrlEnter("c0mX-5q3mrY");
 
-		var videoURLRegex = new RegExp("^(?:https?)?:\\/\\/(?:www\\.)?(?:youtube\\.com\\/watch\\?v=|youtu\\.be\\/)(.+)$");
+		var videoURLRegex = new RegExp(
+			"^(?:https?)?:\\/\\/(?:www\\.)?(?:youtube\\.com\\/watch\\?v=|youtu\\.be\\/)(.+)$"
+		);
 		$("#url").keyup(function() {
 			var url = $("#url").val();
 			var testResults = videoURLRegex.exec(url);
 			if (testResults != null) {
 				$("#url").val(testResults[1]);
 			}
+		});
+		$("#download").click(function() {
+			//TODO: make keyup
+			var url = $("#url").val();
+			onUrlEnter(url);
 		});
 	});
 	function onUrlEnter(videoID) {
@@ -26,7 +32,12 @@
 	}
 
 	function parseInfo(data) {
-		return {
+		data.thumbnailUrl = "https://i.ytimg.com/vi/" + data.video_id + "/maxresdefault.jpg";
+		data.formats = parseFormats(data.formats);
+		return data;
+		/*
+		Officially supported params:
+		{
 			thumbnailUrl: "https://i.ytimg.com/vi/" + data.video_id + "/maxresdefault.jpg",
 			title: data.title,
 			video_id: data.video_id,
@@ -37,16 +48,34 @@
 			description: data.description,
 			video_url: data.video_url
 		};
+		
+		*/
 	}
 
 	function parseFormats(formats) {
-		return {
-			
-		};
+		return {};
 	}
 
 	function displayInfo(videoInfo) {
 		// TODO: videoInfo is what parseInfo() returns
 		console.log(videoInfo);
+		attrs = [
+			"thumbnailUrl",
+			"title",
+			"video_id",
+			"length_seconds",
+			"author",
+			"formats",
+			"published",
+			"description",
+			"video_url"
+		];
+		for (i = 0; i < attrs.length; i++) {
+			$(".videodata-" + attrs[i]).text(videoInfo[attrs[i]]);
+			
+		}
+		$(".src-thumbnailUrl").attr("src", videoInfo.thumbnailUrl);
+		$(".href-thumbnailUrl").attr("href", videoInfo.thumbnailUrl);
+		$(".href-video_url").attr("href", videoInfo.video_url);
 	}
 })(jQuery);
