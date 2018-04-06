d9ab0852743e4ad756d5dfd5c0baed9484ce491f
Nathan Wang <nathan.r.wang@gmail.com>
2018-04-05 21:15:31 -0700

    add basic parsing

--- web/index.js
+++ web/index.js
@@ -3,7 +3,8 @@
 
 	$(document).ready(function() {
 		// TODO:
-		// Add a form with Youtube URL. Add the submit() handler.
+		// Add a form with Youtube URL. On submit, 
+		// call onUrlEnter() with the video id (eg. "cyW2ajAVyfA")
 
 		// Just for testing
 		onUrlEnter("c0mX-5q3mrY");
@@ -18,11 +19,26 @@
 
 	function parseInfo(data) {
 		return {
+			thumbnailUrl: "https://i.ytimg.com/vi/" + data.video_id + "/maxresdefault.jpg",
+			title: data.title,
+			video_id: data.video_id,
+			length_seconds: data.length_seconds,
+			author: data.author,
+			formats: parseFormats(data.formats),
+			published: data.published,
+			description: data.description,
+			video_url: data.video_url
+		};
+	}
 
+	function parseFormats(formats) {
+		return {
+			
 		};
 	}
 
 	function displayInfo(videoInfo) {
-		// TODO
+		// TODO: videoInfo is what parseInfo() returns
+		console.log(videoInfo);
 	}
 })();
