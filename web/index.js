9df177b8eefcc59f18b7236dd2c55ba03f151875
Nathan Wang <nathan.r.wang@gmail.com>
2018-04-05 21:09:28 -0700

    add shell javascript

--- web/index.js
+++ web/index.js
@@ -1,8 +1,28 @@
 (function() {
+	let videoInfo;
+
 	$(document).ready(function() {
 		// TODO:
 		// Add a form with Youtube URL. Add the submit() handler.
+
+		// Just for testing
+		onUrlEnter("c0mX-5q3mrY");
 	});
 
+	function onUrlEnter(videoID) {
+		$.get("/api/getInfo/" + videoID, function(data) {
+			videoInfo = parseInfo(data);
+			displayInfo(videoInfo);
+		});
+	}
 
-});
+	function parseInfo(data) {
+		return {
+
+		};
+	}
+
+	function displayInfo(videoInfo) {
+		// TODO
+	}
+})();
