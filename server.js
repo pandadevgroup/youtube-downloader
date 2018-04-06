5ee8d41d2d9608fd3d7e4b43a1d73615ce22ce27
Nathan Wang <nathan.r.wang@gmail.com>
2018-04-05 22:40:35 -0700

    add youtube search to server

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const path = require('path')
 const ytdl = require('ytdl-core');
+const ytSearch = require("youtube-search");
 const PORT = process.env.PORT || 5000
+
+var searchOpts = {
+	maxResults: 30,
+	key: 'AIzaSyCz3AK9ZY3v-d0ga11E6X1u3vMhIewVFxQ'
+};
 
 express()
 	.use(express.static(path.join(__dirname, 'web')))
@@ -10,6 +16,12 @@ express()
 		ytdl.getInfo(`https://www.youtube.com/watch?v=${req.params.id}`, (err, info) => {
 			if (err) return res.status(500).send(err);
 			res.send(info);
+		});
+	})
+	.get("/api/search/:query", (req, res) => {
+		ytSearch(req.params.query, searchOpts, (err, results) => {
+			if (err) return res.status(500).send(err);
+			res.send(results);
 		});
 	})
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
