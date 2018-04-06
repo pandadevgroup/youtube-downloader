ec7d8f1e8d6480f8d78bf234b0cde4073c6f7fa2
Jeffrey Meng <jeffkmeng@gmail.com>
2018-04-05 21:14:16 -0700

    add basic regex

--- web/index.js
+++ web/index.js
@@ -1,8 +1,22 @@
-(function() {
+(function($) {
 	$(document).ready(function() {
 		// TODO:
 		// Add a form with Youtube URL. Add the submit() handler.
+		var videoURLRegex = new RegExp("^(?:https?)?:\\/\\/(?:www\\.)?(?:youtube\\.com\\/watch?v=|youtu\\.be\\/)(.+)$");
+		console.log(0);
+		$("#url").keyup(function() {
+			console.log(1);
+			var url = $("#url").val();
+			var testResults = videoURLRegex.exec(url);
+			console.log(testResults);
+			if (testResults != null) {
+				console.log(2);
+				$("#url").val(testResults[1]);
+			} else {
+				console.log(testResults);
+			}
+		});
 	});
 
 
-});
+})(jQuery);
