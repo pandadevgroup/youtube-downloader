6f41db28c5e7b74134c995a1564c75d354a71fc7
Nathan Wang <nathan.r.wang@gmail.com>
2018-04-07 12:12:31 -0700

    Add react shell

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const searchOpts = {
 const maxDuration = 60 * 30; // 30 minutes
 
 express()
-	.use(express.static(path.join(__dirname, 'web')))
-	.get('/', (req, res) => res.sendFile(path.join(__dirname, "web", "index.html")))
+	.use(express.static(path.join(__dirname, 'web', "build")))
+	.get('/', (req, res) => res.sendFile(path.join(__dirname, "web", "build", "index.html")))
 	.get("/api/getInfo/:id", (req, res) => {
 		ytdl.getInfo(`https://www.youtube.com/watch?v=${req.params.id}`, (err, info) => {
 			if (err) return res.status(500).send(err);
