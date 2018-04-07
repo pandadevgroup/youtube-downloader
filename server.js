cac38c66f6403b87ea85057cf079ee7d2f97a2dc
Nathan Wang <nathan.r.wang@gmail.com>
2018-04-06 22:13:00 -0700

    add max duration

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const ytSearch = require("youtube-search");
 const ytdlHelper = require("./ytdl");
 const PORT = process.env.PORT || 5000
 
-var searchOpts = {
+const searchOpts = {
 	maxResults: 30,
 	key: 'AIzaSyCz3AK9ZY3v-d0ga11E6X1u3vMhIewVFxQ'
 };
+const maxDuration = 60 * 30; // 30 minutes
 
 express()
 	.use(express.static(path.join(__dirname, 'web')))
@@ -26,13 +27,23 @@ express()
 		});
 	})
 	.get("/api/download/:id", (req, res) => {
-		ytdlHelper.download(req.params.id, "highest").then(download => {
-			res.sendFile(download)
-		}).catch(e => res.status(500).send(e.message));
+		ytdl.getInfo(`https://www.youtube.com/watch?v=${req.params.id}`, (err, info) => {
+			if (err) return res.status(500).send(err);
+			if (info.length_seconds > maxDuration) return res.status(400).send("Max duration is " + maxDuration + " seconds.");
+
+			ytdlHelper.download(req.params.id, "highest").then(download => {
+				res.sendFile(download)
+			}).catch(e => res.status(500).send(e.message));
+		});
 	})
 	.get("/api/download/:id/:itag", (req, res) => {
-		ytdlHelper.download(req.params.id, req.params.itag).then(download => {
-			res.sendFile(download)
-		}).catch(e => res.status(500).send(e.message));
+		ytdl.getInfo(`https://www.youtube.com/watch?v=${req.params.id}`, (err, info) => {
+			if (err) return res.status(500).send(err);
+			if (info.length_seconds > maxDuration) return res.status(400).send("Max duration is " + maxDuration + " seconds.");
+
+			ytdlHelper.download(req.params.id, req.params.itag).then(download => {
+				res.sendFile(download)
+			}).catch(e => res.status(500).send(e.message));
+		});
 	})
 	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
