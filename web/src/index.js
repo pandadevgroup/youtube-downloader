eaf1be56de7638dc08bbddc8860b3d67da86337c
Nathan Wang <nathan.r.wang@gmail.com>
2018-04-08 20:14:41 -0700

    add redux and redux saga

--- web/src/index.js
+++ web/src/index.js
@@ -4,11 +4,17 @@ import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
+
+const store = configureStore();
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+	<Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
